82fe71eca6885fa28bbcc831cbf80859020275a2
juna.k <juna.k@navercorp.com>
2023-04-15 14:48:48 +0900

    feat: 상대가 사용자의 input을 받아 결과를 리턴한다.

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,10 +2,29 @@ package baseball;
 
 public class Computer {
 	private String selectedNumbers;
-	private static final int NUMBER_DIGIT = 3;
 
 	public Computer() {
 		this.selectedNumbers = selectNumbers();
+	}
+
+	public Result getResult(String input) {
+		Result result = new Result();
+		for (int i = 0;i < input.length(); i++) {
+			char inputDigit = input.charAt(i);
+			char computerDigit = this.selectedNumbers.charAt(i);
+			checkResult(result, inputDigit, computerDigit);
+		}
+		return result;
+	}
+
+	private void checkResult(Result result, char inputDigit, char computerDigit) {
+		if (computerDigit == inputDigit) {
+			result.increaseStrike();
+			return;
+		}
+		if (this.selectedNumbers.contains(String.valueOf(inputDigit))) {
+			result.increaseBall();
+		}
 	}
 
 	private String selectNumbers() {
@@ -16,4 +35,28 @@ public class Computer {
 		return number;
 	}
 
+
+	public static class Result {
+		private int strike;
+		private int ball;
+
+		public Result() {
+			this.strike = 0;
+			this.ball = 0;
+		}
+		public void increaseStrike() {
+			this.strike += 1;
+		}
+		public void increaseBall() {
+			this.ball += 1;
+		}
+
+		public int getStrike() {
+			return strike;
+		}
+
+		public int getBall() {
+			return ball;
+		}
+	}
 }
